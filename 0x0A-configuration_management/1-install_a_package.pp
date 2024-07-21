e83f1171932c751f8c6c7325a3f7cc99fd39c326
AbeerRagab7 <abeermali833@gmail.com>
2024-07-21 22:39:20 +0300

    devops

---
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -0,0 +1,21 @@
+# Installing puppet lint
+package { 'puppet-lint':
+  ensure   => '2.1.1',
+  provider => 'gem',
+}
+
+package { 'python3-pip':
+  ensure => installed,
+}
+
+package { 'Flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
+  require  => Package['python3-pip'],
+}
+
+package { 'werkzeug':
+  ensure   => '2.0.1',
+  provider => 'pip',
+  require  => Package['python3-pip'],
+}
